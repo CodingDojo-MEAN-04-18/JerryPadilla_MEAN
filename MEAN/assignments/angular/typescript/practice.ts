a056f1df24f5a06639d041c24c83ab1705b164a1
Jerry Padilla <jerrypadilla23@gmail.com>
2018-05-21 19:53:08 -0500

    assignments

---
+++ MEAN/assignments/angular/typescript/practice.ts
@@ -0,0 +1,13 @@
+class SLNode {
+     val: number;
+
+     constructor(valueP: number){
+          this.val = valueP;
+     }
+     doSomething(){
+          console.log('This is fun');
+     }
+}
+
+let firstSLNode = new SLNode(1);
+
