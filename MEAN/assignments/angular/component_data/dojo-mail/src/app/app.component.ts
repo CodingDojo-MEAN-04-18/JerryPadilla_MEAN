256b14bd0546575044822d9c9872326eba1b3e3f
Jerry Padilla <jerrypadilla23@gmail.com>
2018-05-21 21:04:59 -0500

    dojo mail

--- MEAN/assignments/angular/component_data/dojo-mail/src/app/app.component.ts
+++ MEAN/assignments/angular/component_data/dojo-mail/src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,18 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dojo Mail';
-  emails = {
-    '1': {
-      email: 'bill@gates.com',
-      importance: true,
-      subject: 'New Windows',
-      content: 'Windows XI will launch in year 2010',
+  emails = [{
+       email: 'bill@gates.com',
+       importance: true,
+       subject: 'New Windows',
+       content: 'Windows XI will launch in year 2010'
     },
-    '2': {
-      email: 'ado@lovelace.com',
-      importance: true,
-      subject: 'Programming',
-      content: 'Enchantress of Numbers',
+    {
+         email: 'ado@lovelace.com',
+         importance: true,
+         subject: 'Programming',
+         content: 'Enchantress of Numbers',
     },
-  };
+    {
+         email: 'Rose@lovelace.com',
+         importance: false,
+         subject: 'I love programming',
+         content: 'What is even going on in the right now. I figured it out.',
+    },
+    {
+         email: 'Jerrypadilla23@gmail.com',
+         importance: true,
+         subject: 'Javascript',
+         content: 'Love this Angular stuff so far',
+    }];
 }
